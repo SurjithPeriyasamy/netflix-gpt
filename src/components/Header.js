d205efdeb40f1f4264f19098abf73010e9c2fe44
Surjith <mahindhiran005@gmail.com>
2023-08-27 21:33:18 +0530

    firebase ,router,redux and form validation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO } from "../utils/constants";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const user = useSelector((store) => store.user);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      // Sign In
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            userId: uid,
+            email: email,
+            name: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        //Sign Out
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch(removeUser());
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
   return (
-    <div className=" pl-3 pt-1 ">
-      <img
-        className="w-52"
-        alt="logo"
-        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-      />
+    <div className="px-3 pt-1 flex justify-between items-center">
+      <div className="  ">
+        <img className="w-52" alt="logo" src={LOGO} />
+      </div>
+      {user && (
+        <div className="flex gap-1">
+          <img className="w-14" alt="userimage" src={user.photoURL} />
+          <button onClick={handleSignOut}>Sign out</button>
+        </div>
+      )}
     </div>
   );
 };
